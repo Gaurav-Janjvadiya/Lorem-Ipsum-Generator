60b2009262e97001a7a7e3281d55bc24d0a4bff1
Gaurav-Janjvadiya <janjvadiyagaurav344@gmail.com>
2024-03-02 18:20:21 +0530

    grid

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-// Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+const string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi elit".split(" ");
+
+const textField = document.querySelector(".textField");
+const vals = [...document.querySelectorAll(".val")];
+const range = [...document.querySelectorAll(".range")];
+const items = [...document.querySelectorAll(".item")];
+const select = document.querySelector("select");
+const btn = document.querySelector("button");
+
+range.forEach(item => {
+    item.addEventListener("input",(e) => {
+        if(e.target == range[0]){
+            vals[0].textContent = Number(e.target.value);
+        }
+        if(e.target == range[1]){
+            vals[1].textContent = Number(e.target.value);
+        }
+    })
+})
+
+let swich = 0;
+vals[2].addEventListener("click",() => {
+    if(swich){
+        select.style.display = "none";
+        vals[2].innerHTML = "X";
+        swich = 0;
+    }else{
+        vals[2].innerHTML = '<span class="material-symbols-outlined">check</span>'
+        select.style.display = "flex";
+        swich = 1;
+    }
+})
+
+let text = "";
+
+btn.addEventListener("click",() => {
+    
+})
