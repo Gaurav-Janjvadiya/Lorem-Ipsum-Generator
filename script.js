6fabaeb17a770cdac4b7542cc9c02caffa55a2b7
Gaurav-Janjvadiya <janjvadiyagaurav344@gmail.com>
2024-03-02 22:33:58 +0530

    adding Responsivenes

--- script.js
+++ script.js
@@ -8,31 +8,42 @@ const select = document.querySelector("select");
 const btn = document.querySelector("button");
 
 range.forEach(item => {
-    item.addEventListener("input",(e) => {
-        if(e.target == range[0]){
+    item.addEventListener("input", (e) => {
+        if (e.target == range[0]) {
             vals[0].textContent = Number(e.target.value);
         }
-        if(e.target == range[1]){
+        if (e.target == range[1]) {
             vals[1].textContent = Number(e.target.value);
         }
+        textField.innerHTML = "";
     })
 })
 
 let swich = 0;
-vals[2].addEventListener("click",() => {
-    if(swich){
+vals[2].addEventListener("click", () => {
+    if (swich) {
         select.style.display = "none";
         vals[2].innerHTML = "X";
         swich = 0;
-    }else{
+    } else {
         vals[2].innerHTML = '<span class="material-symbols-outlined">check</span>'
         select.style.display = "flex";
         swich = 1;
     }
 })
+let para = "";
+let finalPara = "";
 
-let text = "";
-
-btn.addEventListener("click",() => {
-    
+btn.addEventListener("click", () => {
+    for (let j = 0; j < Number(vals[0].textContent); j++) {
+        for (let i = 0; i < Number(vals[1].textContent); i++) {
+            para += string[i] + " ";
+        }
+        if (vals[2].innerHTML == "X") {
+            finalPara += `<p>${para}</p><br>`;
+        } else if (0) {}
+        para = "";
+    }
+    textField.innerHTML = finalPara;
+    finalPara = "";
 })
